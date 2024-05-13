19b263fbb612736c71b25575c0fcb2cc60d23b88
Abrarulctg <jubairctg99@gmail.com>
2024-05-13 09:27:10 +0600

    Added find api to find a job by id

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 
 //Mongo Reletaed API Started
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qcsvas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -57,6 +57,14 @@ async function run() {
             res.send(result)
         })
 
+        //find job by id
+        app.get('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        })
+
         //Post a job
         app.post('/jobs', async (req, res) => {
             const newJob = req.body;
@@ -64,6 +72,7 @@ async function run() {
             const result = await jobsCollection.insertOne(newJob);
             res.send(result);
         })
+
 
 
     } finally {
